21592d1fd959380809b34bcac13eaf6c76e856e8
shashwat Dwivedi <itsshashwatdwivedi@gmail.com>
2024-07-14 05:40:01 +0530

    Done with my portfolio website

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
                     Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: "/Shashwat_s_Resume.pdf",
+            href: "/Resume.pdf",
             style: "rounded-br-md",
             download: true,
         },
